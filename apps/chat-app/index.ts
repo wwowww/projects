2295f73e874092082a76ad02a597074ffb16888c
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-03 19:07:33 +0900

    feat. 웹소켓 셋팅

---
+++ apps/chat-app/index.ts
@@ -0,0 +1,17 @@
+const { createServer } = require("http");
+const app = require("./app");
+const { Server } = require("socket.io");
+require("dotenv").config();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
+
+httpServer.listen(process.env.PORT, () => {
+  console.log("server listening on port", process.env.PORT);
+});
+
+require("./utils/io")(io);
