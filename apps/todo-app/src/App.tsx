2bfea9a70ffb44bffa92eb6dcd9177ed99646858
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-22 23:27:14 +0900

    feat. router 추가

--- apps/todo-app/src/App.tsx
+++ apps/todo-app/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+import Layout from "./components/templates/Layout/Layout";
+
+import "./assets/styles/global.scss";
+
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Layout /> 
+    </BrowserRouter>
   )
 }
 
