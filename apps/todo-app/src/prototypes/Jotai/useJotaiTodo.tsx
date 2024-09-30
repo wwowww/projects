211ca00617e575eccd5f2ed80c99ee8f80f91d95
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-30 17:56:54 +0900

    feat. jotai todo list localstorage 설정

--- apps/todo-app/src/prototypes/Jotai/useJotaiTodo.tsx
+++ apps/todo-app/src/prototypes/Jotai/useJotaiTodo.tsx
@@ -1,7 +1,14 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
 import { useCallback } from "react";
 
-const todoListAtom = atom<Todo[]>([]);
+const localStorageKey = 'jotaiTodoStore';
+
+const getTodosFromLocalStorage = (): Todo[] => {
+  const storedTodos = localStorage.getItem(localStorageKey);
+  return storedTodos ? JSON.parse(storedTodos) : [];
+};
+
+const todoListAtom = atom<Todo[]>(getTodosFromLocalStorage());
 
 export const useJotaiTodoValue = () => {
   return useAtomValue(todoListAtom);
@@ -11,18 +18,30 @@ export const useJotaiTodoMutations = () => {
   const setTodos = useSetAtom(todoListAtom);
   
   const addTodo = useCallback((todo: Todo) => {
-    setTodos((prev) => [...prev, todo]);
-  }, [setTodos])
+    setTodos((prev) => {
+      const newTodos = [...prev, todo];
+      localStorage.setItem(localStorageKey, JSON.stringify(newTodos));
+      return newTodos;
+    });
+  }, [setTodos]);
 
   const removeTodo = useCallback((id: string) => {
-    setTodos((prev) => prev.filter((prev) => prev.id !== id))
-  }, [setTodos])
+    setTodos((prev) => {
+      const newTodos = prev.filter((todo) => todo.id !== id);
+      localStorage.setItem(localStorageKey, JSON.stringify(newTodos));
+      return newTodos;
+    });
+  }, [setTodos]);
 
   const updateTodo = useCallback((todo: Todo) => {
-    setTodos((prev) => prev.map((prev) => {
-      return prev.id === todo.id ? todo : prev;
-    }));
-  }, [setTodos])
+    setTodos((prev) => {
+      const newTodos = prev.map((prevTodo) => {
+        return prevTodo.id === todo.id ? todo : prevTodo;
+      });
+      localStorage.setItem(localStorageKey, JSON.stringify(newTodos));
+      return newTodos;
+    });
+  }, [setTodos]);
 
   return {
     addTodo,
@@ -30,4 +49,3 @@ export const useJotaiTodoMutations = () => {
     updateTodo
   }
 }
-
