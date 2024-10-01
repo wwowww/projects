c6daa08436fee286ad04715fc0358e43a0ffed96
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-01 14:47:16 +0900

    feat. typography type 추가

---
+++ apps/todo-app/src/types/Typography.ts
@@ -0,0 +1,12 @@
+export type TypographyVariants = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+
+export type TypographyCode = 
+"title-40-140-600"
+| "title-32-140-700"
+| "title-14-140-400"
+| "title-20-150-400"
+| "body-24-140-600"
+| "body-18-150-400"
+| "body-16-150-400"
+| "body-14-150-400"
+| "label-12-100-600";
