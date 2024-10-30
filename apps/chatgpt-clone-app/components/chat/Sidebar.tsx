b1e1b5d2323a94834121163740b6228d83b8e2ec
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-30 16:03:15 +0900

    feat. 사이드바 대화 페이지 db 연동

--- apps/chatgpt-clone-app/components/chat/Sidebar.tsx
+++ apps/chatgpt-clone-app/components/chat/Sidebar.tsx
@@ -1,39 +1,36 @@
-
 import { MessageSquare, Plus } from "lucide-react";
 import { BASE_URL, CHAT_ROUTES } from "@/constants/routes";
 import Logo from "./Logo";
 import SidebarItem from "./SidebarItem";
 import LogoutButton from "./LogoutButton";
+import { getConversationsByUser } from "@/data/user";
 
-const DUMMY_ITEMS = [
-  {
-    id: "new",
-    label: "새로운 대화",
-    icon: <Plus />,
-    href: BASE_URL
-  },
-  {
-    id: "1",
-    label: "새로운 긴 대화 예시입니다. 새로운 긴 대화 예시입니다. 새로운 긴 대화 예시입니다. 새로운 긴 대화 예시입니다. ",
-    icon: <MessageSquare />,
-    href: `${CHAT_ROUTES.CONVERSATIONS}/1`
-  },
-  {
-    id: "2",
-    label: "일반 대화 예시입니다.",
-    icon: <MessageSquare />,
-    href: `${CHAT_ROUTES.CONVERSATIONS}/2`
-  }
+const NEW_SIDEBAR_ITEM = {
+  id: "new",
+  label: "새로운 대화",
+  icon: <Plus />,
+  href: BASE_URL
+};
 
-]
+const Sidebar = async () => {
+  const conversations = await getConversationsByUser();
 
-const Sidebar = () => {
+  const formattedItems = [
+    NEW_SIDEBAR_ITEM,
+    ...conversations.map((conversation) => ({
+      id: conversation.id,
+      label: conversation.name || "",
+      icon: <MessageSquare />,
+      href: `${CHAT_ROUTES.CONVERSATIONS}/${conversation.id}`,
+    })),
+  ];
+
   return (
     <nav className="h-full p-3 bg-black flex flex-col text-white">
       <div className="flex-1 overflow-y-auto">
       <Logo />
         <div className="flex flex-col gap-2 mt-10">
-          {DUMMY_ITEMS.map((item) => (
+          {formattedItems.map((item) => (
             <SidebarItem key={item.id} item={item} />
           ))}
         </div>
--- apps/chatgpt-clone-app/data/user.ts
+++ apps/chatgpt-clone-app/data/user.ts
@@ -1,5 +1,6 @@
+import { verifySession } from "@/actions/sessions";
 import { db } from "@/db";
-import { user } from "@/db/schema";
+import { conversation, user } from "@/db/schema";
 import { User } from "@/types/db";
 import { eq } from "drizzle-orm";
 
@@ -16,4 +17,19 @@ export const getUserByEmail = async (email: string): Promise<User | null> => {
     console.error(error, "error");
     throw new Error("문제가 발생했습니다.");
   }
+}
+
+export const getConversationsByUser = async() => {
+  const session = await verifySession();
+
+  const response = await db.query.user.findFirst({
+    where: eq(user.id, session.id),
+    with: {
+      conversations: {
+        orderBy: (conversation, { desc }) => [desc(conversation.updatedAt)],
+      },
+    },
+  })
+
+  return response?.conversations || [];
 }
