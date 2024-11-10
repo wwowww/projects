c2786a43fef60aab8bccc03e073c40b0838376b4
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-10 16:33:25 +0900

    feat. 메세지 순서 내림차순(desc)에서 오름차순(asc)으로 변경

--- apps/chatgpt-clone-app/data/conversation.ts
+++ apps/chatgpt-clone-app/data/conversation.ts
@@ -2,12 +2,13 @@ import { db } from "@/db";
 import { conversation } from "@/db/schema";
 import { Message } from "ai";
 import { eq } from "drizzle-orm";
+import { message } from '../db/schema';
 export const getMessagesByConversation = async (id: string) => {
   const response = await db.query.conversation.findFirst({
     where: eq(conversation.id, id),
     with: {
       message: {
-        orderBy: (conversation, { desc }) => [desc(conversation.updatedAt)],
+        orderBy: (message, { asc }) => [asc(message.createdAt)],
       },
     },
   });
